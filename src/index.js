b55a38e656cb3601e7420a9bba5750309a0285d4
kwanik-kor <rhksdlr134@naver.com>
2021-01-10 16:23:16 +0900

    1. React 소개 완료

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+import SearchBar from './components/search_bar';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+const API_KEY = 'AIzaSyBimS54EI4UTX0u-X9gzh0lPehKw0AVLAs';
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Create a new component. This component should porduce some HTML
+// 'const' came out from 'ES6'
+//  => const means constant and this is the final value
+const App = () => {
+    return (
+    <div>
+        <SearchBar />
+    </div>
+    );
+}
+
+// "React! Plz take this components that I just made put it into DOM!"s
+ReactDOM.render(<App />, document.querySelector('.container'));
